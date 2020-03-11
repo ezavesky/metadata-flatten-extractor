b89a1e387506df114ec78d3235ccf53b41d64a39
Zavesky, Eric (ez2685) <ez2685@att.com>
2020-03-10 23:08:32 -0500

    small fix for face attributes when not present

--- app/modes/event_table.py
+++ app/modes/event_table.py
@@ -103,7 +103,7 @@ def main_sidebar(df, sort_list=None):
     # Only show the slider if there is more than one value for that slider, otherwise, don't filter
 
     type_unique = ["All"] + list(df["tag_type"].unique())
-    filter_tag = st.sidebar.selectbox("Tag Type for Exploration", type_unique, index=type_unique.index('identity'))
+    filter_tag = st.sidebar.selectbox("Event Type", type_unique, index=type_unique.index('identity'))
     df_sub = df
     if filter_tag != "All":
         df_sub = df[df['tag_type'] == filter_tag]
@@ -111,7 +111,7 @@ def main_sidebar(df, sort_list=None):
 
     # strict tag source filter
     source_filter = ["All"] + list(df_sub["source_event"].unique())
-    source_tag = st.sidebar.selectbox("Tag Type for Exploration", source_filter, index=0)
+    source_tag = st.sidebar.selectbox("Event Source", source_filter, index=0)
     if source_tag != "All":
         idx_match &= (df_sub['source_event'] == source_tag)
 
