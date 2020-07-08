3d4ed7e95cb9fe91a4cb1dc31844bf464397236d
Eric Z <ezavesky@research.att.com>
2020-07-08 14:28:55 -0500

    update multi-app / quality experiment

--- metadata_flatten/_version.py
+++ metadata_flatten/_version.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __package__ = 'metadata_flatten'
 __description__ = "Metadata Flattening Service"
 __copyright__ = "Copyright AT&T Services and Warner Media 2020"
